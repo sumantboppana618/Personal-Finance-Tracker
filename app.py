a9d9147c86969cc67cbe5b66b1e73f0c1562a6ba
Sumant Boppana <sumantboppana618@gmail.com>
2026-04-06 22:17:50 +0300

    Fix app import order for flake8

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-load_dotenv()
 from flask import Flask, jsonify, render_template, request
 
 from db import (
@@ -12,6 +11,8 @@ from db import (
     get_transactions,
     update_transaction,
 )
+
+load_dotenv()
 
 app = Flask(__name__)
 
